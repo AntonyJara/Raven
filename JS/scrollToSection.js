635f7a5894e3c5642bf7d531a2c94941d931590c
AntonyJara <89340033+AntonyJara@users.noreply.github.com>
2023-11-24 12:39:46 -0500

    Add files via upload

--- JS/scrollToSection.js
+++ JS/scrollToSection.js
@@ -1,6 +1,6 @@
 function scrollToSection() {
     // Obtener el elemento de destino por su ID
-    var destino = document.getElementById('destino');
+    var destino = document.getElementById('Information');
 
     // Hacer el desplazamiento suave hasta el elemento de destino
     destino.scrollIntoView({ behavior: 'smooth' });
